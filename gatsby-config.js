57103e04d5a2dd4ed8d0c6aac2395a8b9b2f16cd
James Troughton <jamesstroughton@gmail.com>
2021-01-12 13:35:27 +1300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `React Website V1`,
+    description: `???`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    
     {
       resolve: `gatsby-source-filesystem`,
       options: {
---
+++ src/data/MenuData.js
@@ -0,0 +1,19 @@
+export const menuData = [
+  {
+    title: "About",
+    link: "/about",
+  },
+
+  {
+    title: "Portfolio",
+    link: "/portfolio",
+  },
+  {
+    title: "Blog",
+    link: "/blog",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+]
